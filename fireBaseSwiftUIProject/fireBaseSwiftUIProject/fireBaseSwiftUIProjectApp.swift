de99997723baa324c9162308170ab69c995898e4
Ainur <sibrinat616@gmail.com>
2024-03-15 21:25:34 +0300

    added fireBase package

---
+++ fireBaseSwiftUIProject/fireBaseSwiftUIProject/fireBaseSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  fireBaseSwiftUIProjectApp.swift
+//  fireBaseSwiftUIProject
+//
+//  Created by Ainur on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct fireBaseSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
